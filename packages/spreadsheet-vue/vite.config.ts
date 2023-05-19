86c81da81e228ff1bd5a481b233d93483459a41e
xbzhang2020 <xbzhang2020@icloud.com>
2023-05-19 23:31:18 +0800

    feat(all): vite打包

--- packages/spreadsheet-vue/vite.config.ts
+++ packages/spreadsheet-vue/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "SpreadSheet",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        dir: "lib",
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
