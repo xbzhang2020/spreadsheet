80dd0df7c2a6c7eb65fb0c3f65a124684c6e9717
xbzhang2020 <xbzhang2020@icloud.com>
2023-05-20 16:56:52 +0800

    feat: import element plus

--- packages/spreadsheet-vue/src/main.ts
+++ packages/spreadsheet-vue/src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
 
-createApp(App).mount('#app')
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+
+app.mount("#app");
