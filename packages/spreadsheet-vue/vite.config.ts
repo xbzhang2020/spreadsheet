66823ffc1bd53570b31094155f27a08156778359
xbzhang2020 <xbzhang2020@icloud.com>
2023-06-04 09:57:28 +0800

    refactor: 代码重构

--- packages/spreadsheet-vue/vite.config.ts
+++ packages/spreadsheet-vue/vite.config.ts
@@ -1,7 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import _default from "element-plus/es/locale/lang/af";
 // import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
