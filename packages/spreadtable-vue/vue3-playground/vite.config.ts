e09e16d853bfce61901e80f6ba4bb76548d53281
xbzhang2020 <xbzhang2020@icloud.com>
2023-06-05 08:12:01 +0800

    feat: build

--- packages/spreadtable-vue/vue3-playground/vite.config.ts
+++ packages/spreadtable-vue/vue3-playground/vite.config.ts
@@ -4,4 +4,24 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: "../dist/v3",
+    emptyOutDir: false,
+    lib: {
+      entry: "./src/main.ts",
+      name: "Spreadsheet",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ["vue-demi"],
+  },
 });
